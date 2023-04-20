56b94bb276111ff17d56fd33f29b8798e5775780
Aidasvenc <79483254+Aidasvenc@users.noreply.github.com>
2023-04-20 10:48:18 +0200

    created aggregator class

---
+++ aggregator/aggregator.py
@@ -0,0 +1,51 @@
+import sys
+from typing import Dict, List, Optional, Tuple
+from blspy import (PrivateKey, AugSchemeMPL, G1Element, G2Element)
+from chia_blockchain.chia.types.spend_bundle import SpendBundle
+from chia_blockchain.chia.wallet.transaction_record import TransactionRecord
+sys.path.append('../chia_blockchain')
+
+
+class Aggregator:
+    """
+    Has to have an array of Transactions(SpendBundle + additional parameters) that it can possibly aggregate
+    We have 2 choices:
+    1. Fill the array from the mempool by calling create_bundle_from_mempool() that will also aggregate many
+    SpendBundles into one SpendBundle with a multi-sig
+    2. Fill the array gradually by receiving requests that contain transactions(or SpendBundles)
+    """
+
+    signed_transactions: List[SpendBundle]  # use generate_signed_transaction() from wallet_tools
+    batch_threshold: int  # number of transactions that are aggregated into a batch
+    batch: List[SpendBundle]  # batch of transactions to be aggregated
+    digest: bytearray  # digest of a batch in a form of Merkle Tree
+    signed_digest: List[G2Element]  # all signatures of digest
+    agg_digest: G2Element  # aggregated digest signatures
+
+    def __init__(self, batch_threshold: int):
+        self.signed_transactions = List[SpendBundle]()
+        self.batch_threshold = batch_threshold
+        self.batch = List[SpendBundle]()
+        self.digest = bytearray()
+        self.signed_digest = List[G2Element]()
+        self.agg_digest = G2Element()
+
+    def receive_transaction(self):
+        """
+        TODO: beginning to be defined by Alberto
+        """
+
+
+    def create_batch(self):
+        """
+        creates a batch from the SpendBundles that were accumulated
+        """
+        if len(self.signed_transactions) > self.batch_threshold:
+            self.batch = self.signed_transactions[:]
+
+        return generate_digest()
+
+    def generate_digest(self):
+        """
+        generates a merkle tree from the
+        """
--- aggregator/main.py
+++ aggregator/main.py
@@ -1,14 +1,30 @@
-# This is a sample Python script.
+from blspy import (PrivateKey, AugSchemeMPL, G1Element, G2Element)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # Seed for private key generation (has to be random and 32 bytes or more)
+    seed: bytes = bytes([0, 50, 6, 244, 24, 199, 1, 25, 52, 88, 192,
+                         19, 18, 12, 89, 6, 220, 18, 102, 58, 209, 82,
+                         12, 62, 89, 110, 182, 9, 44, 20, 254, 22])
+
+    # generate some private keys and messages
+    message: bytes = bytes([1, 2, 3, 4, 5])
+    seed = bytes([1]) + seed[1:]
+    sk1: PrivateKey = AugSchemeMPL.key_gen(seed)
+    seed = bytes([2]) + seed[1:]
+    sk2: PrivateKey = AugSchemeMPL.key_gen(seed)
+    message2: bytes = bytes([1, 2, 3, 4, 5, 6, 7])
+
+    # Generate first sig
+    pk1: G1Element = sk1.get_g1()
+    sig1: G2Element = AugSchemeMPL.sign(sk1, message)
+
+    # Generate second sig
+    pk2: G1Element = sk2.get_g1()
+    sig2: G2Element = AugSchemeMPL.sign(sk2, message2)
+
+    # Signatures can be non-interactively combined by anyone
+    agg_sig: G2Element = AugSchemeMPL.aggregate([sig1, sig2])
+
+    ok = AugSchemeMPL.aggregate_verify([pk1, pk2], [message, message2], agg_sig)
+    print(ok)
+    assert ok
